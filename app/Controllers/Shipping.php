c8d1429b164fe7811206bd0bd1f77074fbde0475
Ahmad Faris <6426328-Faris123@users.noreply.gitlab.com>
2021-11-12 09:58:11 +0700

    fungsi shipping

--- app/Controllers/Shipping.php
+++ app/Controllers/Shipping.php
@@ -37,6 +37,55 @@ class Shipping extends BaseController
 		$data = $this->request->getPost();
 		print_r($data);
 		
+		$pro = $this->request->getPost('provinsi');
+		$kot = $this->request->getPost('kota');
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://api.rajaongkir.com/starter/city?id=$kot&province=$pro",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "GET",
+			CURLOPT_HTTPHEADER => array(
+				"key: ".$this->apiKey
+			),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		// print_r ($response);
+
+		//cost
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => "origin=501&destination=$kot&weight=1000&courier=jne",
+			CURLOPT_HTTPHEADER => array(
+				"content-type: application/x-www-form-urlencoded",
+				"key: ".$this->apiKey
+			),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		print_r ($response);		
 	}
 
 	public function getCity()
